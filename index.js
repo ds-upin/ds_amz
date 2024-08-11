feeb5e0b0b1eab7e2e64c860b3a5cf48cdc60984
ds-upin <divyanshu.singh.bzzf@gmail.com>
2024-08-11 14:54:09 +0530

    Added scroll feature

---
+++ index.js
@@ -0,0 +1,11 @@
+function my_search_down(){
+    var a = document.getElementById("options");
+    if (a.style.display === "none")
+    {
+        a.style.display = "block";
+    }
+    else
+    {
+        a.style.display = "none";
+    }
+}
